47e67cec538b9fc834dc295ca2df8dfbe61bd76d
vaibhav vemani <vaibhavvemani@gmail.com>
2024-12-01 19:35:16 +0530

    structure push

---
+++ app/pen-detection/yolo.py
@@ -0,0 +1,16 @@
+from ultralytics import YOLO
+import cv2
+
+
+model = YOLO('yolov8n.pt')
+results = model('Dataset/pen.jpg')
+
+# Process results list
+for result in results:
+    boxes = result.boxes  # Boxes object for bounding box outputs
+    masks = result.masks  # Masks object for segmentation masks outputs
+    keypoints = result.keypoints  # Keypoints object for pose outputs
+    probs = result.probs  # Probs object for classification outputs
+    obb = result.obb  # Oriented boxes object for OBB outputs
+    result.show()  # display to screen
+    result.save(filename="result.jpg")  # save to diskfor re
